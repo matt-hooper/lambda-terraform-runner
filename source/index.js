c38b46fc317affa52f12a84cb8609e0ae5350495
matt-hooper <mhooper@scottlogic.com>
2019-06-10 10:13:23 +0100

    Tidy up lambda function

--- source/index.js
+++ source/index.js
@@ -3,16 +3,42 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const extract = require('extract-zip');
 
-const TERRAFORM_OPT = '/opt/terraform_app/terraform';
+const TERRAFORM_APP_LAYER_LOC = '/opt/terraform_app/terraform';
 const TERRAFORM = '/tmp/terraform';
-const TERRAFORM_PLUGIN_OPT = '/opt/terraform_plugin/.terraform/plugins/linux_amd64';
+const TERRAFORM_PLUGIN_LAYER_LOC = '/opt/terraform_plugin/.terraform/plugins/linux_amd64';
 const TERRAFORM_PLUGIN = '/tmp/linux_amd64';
+const SHARED_MODULES_KEY = 'shared-modules';
 
 
 // get reference to S3 client 
 const s3 = new AWS.S3();
 
-const writeAndUnPackFile = async (data, file_key) => {
+// setup terraform 
+const setupTerraform = () => {
+    sh.exec(`cp ${TERRAFORM_APP_LAYER_LOC} ${TERRAFORM}`);
+    sh.exec(`cp -r ${TERRAFORM_PLUGIN_LAYER_LOC} ${TERRAFORM_PLUGIN}`);
+    sh.exec(`chmod +x ${TERRAFORM}`);
+    sh.exec(`chmod -R +x ${TERRAFORM_PLUGIN}`);
+    console.log(sh.exec(`${TERRAFORM} -version`, {silent:true}).stdout);
+}
+
+const downloadZipFromS3AndUnPack = async (bucket, workspace, file_key) => {
+    const params = {
+      Bucket: `${bucket}`, 
+      Key: `${workspace}-${file_key}.zip`
+    };
+    
+    try {
+        const data = await s3.getObject(params).promise();
+        await writeAndUnpackFile(data, file_key);
+    }
+    catch (err) {
+        console.log(`An error occurred during download. Bucket: ${bucket}. File: ${file_key}.zip.`, err, err.stack); // an error occurred
+        throw new Error('Download error.'); 
+    };
+} 
+
+const writeAndUnpackFile = async (data, file_key) => {
     const zipFile = `/tmp/${file_key}.zip`;
     const target = `/tmp/${file_key}`;
     fs.writeFileSync(zipFile, data.Body);
@@ -25,24 +51,18 @@ const writeAndUnPackFile = async (data, file_key) => {
         });
     }
     catch(err) {
-        console.log(err);
+        console.log(`An error occurred during Unpack. File: ${file_key}.zip.`, err, err.stack); // an error occurred
+        throw new Error('Unpack error.');
     }
     
 }
 
-const processS3Object = (data, action, workspace, script_key) => {
-    //fs.writeFileSync('/tmp/terraform-script.tf', data.Body); 
+const runTerraform = (action, workspace, script_key) => {
+    
     sh.cd(`/tmp/${script_key}`);
-    //sh.cd('/tmp/shared-modules');
-    //sh.cd(`/tmp`);
-    //sh.exec('ls');
-    //sh.exec('ls -la /opt/terraform_app');
     sh.exec(`${TERRAFORM} init -input=false -force-copy -plugin-dir=${TERRAFORM_PLUGIN}`);
-    //sh.exec(`${TERRAFORM} workspace delete ${workspace}`);
     sh.exec(`${TERRAFORM} workspace select ${workspace} || ${TERRAFORM} workspace new ${workspace}`);
-    //sh.exec(`${TERRAFORM} init -input=false -force-copy`);
     sh.exec(`${TERRAFORM} ${action} -auto-approve -lock=false`);
-    //sh.exec(`${TERRAFORM} plan`);
  }
 
 exports.handler = async (event, context) => {
@@ -50,66 +70,21 @@ exports.handler = async (event, context) => {
     const action = event.action || 'apply';
     const script_key = event.script_key || 'simple';
     const workspace = event.workspace || 'lambda';
-    const bucket = 'dec-mhooper-terraform-scripts';
+    const bucket = event.bucket_name || 'dec-mhooper-terraform-scripts';
     
     let scriptCount = 0;
     
     console.log(`Action: ${action}. Script: ${script_key}. Workspace: ${workspace}`);
     
-    sh.exec(`cp ${TERRAFORM_OPT} ${TERRAFORM}`);
-    sh.exec(`cp -r ${TERRAFORM_PLUGIN_OPT} ${TERRAFORM_PLUGIN}`);
-    sh.exec(`chmod +x ${TERRAFORM}`);
-    sh.exec(`chmod -R +x ${TERRAFORM_PLUGIN}`);
-    console.log(sh.exec(`${TERRAFORM} -version`, {silent:true}).stdout);
+    setupTerraform();
     
-    let data; 
-    let params;
+    // download shared modules
+    await downloadZipFromS3AndUnPack(bucket, workspace, SHARED_MODULES_KEY);
     
-    const shared_key = 'shared-modules';
-    
-    params = {
-      Bucket: `${bucket}`, 
-      Key: `${shared_key}.zip`
-    };
-    
-    try {
-        data = await s3.getObject(params).promise();
-        await writeAndUnPackFile(data, shared_key);
-    }
-    catch (err) {
-        console.log('an error occurred', err, err.stack); // an error occurred
-        throw new Error('an error occurred'); 
-    };
-    
-    params = {
-      Bucket: `${bucket}`, 
-      Key: `${script_key}.zip`
-    };
-    
-    
-    try {
-        data = await s3.getObject(params).promise();
-        await writeAndUnPackFile(data, script_key);
-    }
-    catch (err) {
-        console.log('an error occurred', err, err.stack); // an error occurred
-        throw new Error('an error occurred'); 
-    };
-    
-    params = {
-      Bucket: `${bucket}`, 
-      Key: `simple.tf`
-    };
-    
-    try {
-        data = await s3.getObject(params).promise();
-    }
-    catch (err) {
-        console.log('an error occurred', err, err.stack); // an error occurred
-        throw new Error('an error occurred'); 
-    };
-    
-    processS3Object(data, action, workspace, script_key);
+    // download target script
+    await downloadZipFromS3AndUnPack(bucket, workspace, script_key);
+
+    runTerraform(action, workspace, script_key);
     scriptCount++;
         
     console.log(`Scripts processed: `, scriptCount);   
@@ -123,5 +98,3 @@ exports.handler = async (event, context) => {
     };
     return response;
 };
-
-
